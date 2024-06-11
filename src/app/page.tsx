0ec7c606b2bd49fbe5c401570514e776360cd921
Michael Omudho <mikemudho@gmail.com>
2024-06-11 16:10:22 +0200

    intro 15 min

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { Check } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-center p-56">
-      <h1 className="text-blue-500 font-bold text-4xl">
-        This is Michelles <span className="text-6xl text-orange-400">big</span> Nyash
-      </h1>
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:pt-4">
+            <div className="relati e mx-auto text-center flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <img src="/snake-1.png" alt="" />
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>
+                Phone Case
+              </h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">Capture your favourite memomry with your own <span>one-of-one</span>phone case.Casecobra allows you to protect your memories, not just your phone case.</p>
+
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600"/>
+                    High-quality,durable material
+                  </li> 
+                </div> 
+              </ul>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
     </div>
   );
 }
