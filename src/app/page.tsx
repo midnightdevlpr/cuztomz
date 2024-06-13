8a16a9ac65519e85e93823206571b8a189ddaff6
Michael Omudho <mikemudho@gmail.com>
2024-06-13 17:12:35 +0300

    1 hr done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
+import Phone from "@/components/Phone";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,8 +8,8 @@ export default function Home() {
     <div className="bg-slate-50">
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
-          <div className="col-span-2 px-6 lg:pt-4">
-            <div className="relati e mx-auto text-center flex flex-col items-center lg:items-start">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
                 <img src="/snake-1.png" alt="" />
               </div>
@@ -67,12 +68,35 @@ export default function Home() {
                     src="/users/user-5.jpg"
                     alt="user image"
                   />
-                </div> 
+                </div>
+
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                    <Star className="h-4 w-4 text-green-600 fill-green-600" />
+                  </div>
+
+                  <p>
+                    <span className="font-semibold">1,250</span>Happy Customers
+                  </p>
+                </div>
               </div>
+            </div>
+            </div> 
+
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img src="/your-image.png" className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden lg:hidden xl:block"/>
+              <img src="/line.png" className="absolute w-20 -left-6 select-none" />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
-      </section>
+      </section> 
     </div>
   );
 }
